90af5e0881674e7e3aec1fbb52c550bc850340ae
Pedro Pacheco <pedropachecoasencio90@gmail.com>
2022-09-27 16:58:06 -0600

    :bento: add new element slide range

--- src/js/script.js
+++ src/js/script.js
@@ -136,3 +136,98 @@ Drageable.addEventListener('mousemove', (e) =>{
 });
 
 //Scroll Drageable
+
+
+//Range
+
+const inputRange = $('#html-input-range');
+let htmlInputRange = {
+  idNotThere : '<p>Note: Missing input tag id name e.g html-input-range</p>',
+  init () {
+    if (inputRange.length > 0) {
+      let inputRanges = $('#html-input-range');
+      inputRanges.parent().addClass('html-inupt-range');
+      // if too many input ranges there for now hiding this part.
+      // inputRanges.each(function( index ) {
+      //   let $this = $(this);
+      // });
+      let $this = inputRanges;
+        // default input range starts at 0 and ends at 100
+        $this.attr({
+          min: 0,
+          max: 100,
+          value: 0,
+          step: 1
+        });
+    } else {
+      $('input[type=range]').parent().append(htmlInputRange.idNotThere);
+    }
+  },
+  options (inputs) {
+    htmlInputRange.init();
+    let options = inputs;
+    // custom tracker
+    $('input[type=range]').parent().addClass('html-input-range-custom');
+    $('input[type=range]').parent().append('<div class="hir-tracker-bg"></div><div class="hir-tracker-thumb"></div>');
+    
+    let min = 0;
+    let max = 100;
+
+    if (options.tooltip === true) {
+      if (options.max && options.min) {
+        max = options.max;
+        min = options.min;
+
+        $(inputRange).attr({
+          max: options.max,
+          min: options.min
+        });
+      }
+      
+      $('input[type=range]').parent().append('<div class="tooltip">'+ min +'</div>');
+    }
+
+    if (options.labels === true) {
+      $(inputRange).parent().append('<ul class="hir-labels"></ul>');
+      let setWidth;
+      if (options.max) {
+        setWidth = options.max/10;
+      } else {
+        setWidth = max/10; 
+      }
+      for (let i = 0; i < setWidth; i++) {
+        $('.hir-labels').append('<li class="col-'+ setWidth +' "></li>');
+      }
+    }
+  }
+}
+
+$(inputRange).on('input change', inputRange, function (e) {
+
+  let inputMin = parseInt(inputRange.attr('min'));
+  let inputMax = parseInt(inputRange.attr('max'));
+  let selectedValue = inputRange.val();
+
+  let distance = (selectedValue - inputMin) / Math.abs(inputMin - inputMax);
+
+  let trackerTooltipMove = (distance * 100);
+  $('.html-inupt-range .tooltip').css('left', trackerTooltipMove + '%');
+  $('.html-input-range-custom .hir-tracker-thumb').css('width', trackerTooltipMove + '%');
+  // updating tooltip value based on the range from to.
+  $('.html-inupt-range .tooltip').text(selectedValue);
+});
+
+
+$(document).ready(function () {
+  // initiating html input range plugin
+  // htmlInputrange.default();
+  // if you want to customize html input range plugin
+  htmlInputRange.options({
+    tooltip: true,
+    min: 1,
+    max: 10,
+    value: 1,
+  });
+});
+
+//Range
